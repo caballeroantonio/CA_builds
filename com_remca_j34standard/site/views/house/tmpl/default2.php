8d2f1bc5e4fa916b36aa2d320ef543db48fcdf1f
caballeroantonio <caballeroantonio@hotmail.com>
2018-02-12 10:46:10 -0600

    @ToDo terminar vista singular

--- com_remca_j34standard/site/views/house/tmpl/default2.php
+++ com_remca_j34standard/site/views/house/tmpl/default2.php
@@ -33,142 +33,7 @@ if ($realestatemanager_configuration['location_tab']['show']
     || $realestatemanager_configuration['street_view']['show']) {
   $api_key = $realestatemanager_configuration['api_key'] ? "key=" . $realestatemanager_configuration['api_key'] : JFactory::getApplication()->enqueueMessage("<a target='_blank' href='//developers.google.com/maps/documentation/geocoding/get-api-key'>" . JText::_('_REALESTATE_MANAGER_GOOGLEMAP_API_KEY_LINK_MESSAGE') . "</a>", JText::_('_REALESTATE_MANAGER_GOOGLEMAP_API_KEY_ERROR')); 
   $this->document->addScript("//maps.googleapis.com/maps/api/js?{$api_key}");
-  if($realestatemanager_configuration['street_view']['show']){
-    ?>
-    <script type="text/javascript">  
-      jQuery( initialize );
-      var map;
-      var myLatlng=new google.maps.LatLng(<?php
-        if ($this->item->hlatitude && $this->item->hlatitude != '')
-          echo $this->item->hlatitude;
-        else
-          echo 0;
-        ?>,<?php
-        if ($this->item->hlongitude && $this->item->hlongitude != '')
-          echo $this->item->hlongitude;
-        else
-          echo 0;
-        ?>);
-      var sv = new google.maps.StreetViewService();
-
-      var panorama;
-      function initialize(){
-        var myOptions = {
-            zoom: <?php if ($this->item->map_zoom)
-                            echo $this->item->map_zoom;
-                        else
-                            echo 1;
-                        ?>,
-            center: myLatlng,
-            scrollwheel: false,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.LARGE
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        if(document.getElementById("map_canvas") != undefined){
-          map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        }
-        var imgCatalogPath = "<?php echo $mosConfig_live_site;
-         ?>/components/com_realestatemanager/";
-  <?php
-        $newArr = explode(",", JText::_('_REALESTATE_MANAGER_HOUSE_MARKER'));
-        $numPick = '';
-        if (isset($newArr[$this->item->property_type])) {
-            $numPick = $newArr[$this->item->property_type];
-        }
-  ?>
-        var srcForPic = "<?php echo $numPick; ?>";
-        var image = '';
-        if(srcForPic.length){
-          var image = new google.maps.MarkerImage(imgCatalogPath + srcForPic,
-          new google.maps.Size(32, 32),
-          new google.maps.Point(0,0),
-          new google.maps.Point(16, 32));
-        }
-        var marker = new google.maps.Marker({ icon: image,position: myLatlng });
-        marker.setMap(map);
-        var panoramaOptions = {
-          position: myLatlng,
-          pov: {
-            heading: 34,
-            pitch: 10
-          }
-        };
-        var streetViewService = new google.maps.StreetViewService();
-        var STREETVIEW_MAX_DISTANCE = 50;
-        streetViewService.getPanoramaByLocation(myLatlng, STREETVIEW_MAX_DISTANCE, function (streetViewPanoramaData, status) {
-          if (status === google.maps.StreetViewStatus.OK) {
-            // ok
-            var panorama = new google.maps.StreetViewPanorama(document.getElementById('map_pano'), panoramaOptions);
-            map.setStreetView(panorama);
-          } else {
-            document.getElementById('map_pano').style.display = "none";
-            // no street view available in this range, or some error occurred
-          }
-        });
-
-      }
-    </script>
-    <?php
-
-  }else{
-
-    ?>
-    <script type="text/javascript">  
-
-      jQuery( initialize );
-      var map;
-      var myLatlng=new google.maps.LatLng(<?php
-        if ($this->item->hlatitude && $this->item->hlatitude != '')
-          echo $this->item->hlatitude;
-        else
-          echo 0;
-        ?>,<?php
-        if ($this->item->hlongitude && $this->item->hlongitude != '')
-          echo $this->item->hlongitude;
-        else
-          echo 0;
-        ?>); 
-      function initialize(){
-        var myOptions = {
-            zoom: <?php if ($this->item->map_zoom)
-                            echo $this->item->map_zoom;
-                        else
-                            echo 1;
-                        ?>,
-            center: myLatlng,
-            scrollwheel: false,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.LARGE
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-        var imgCatalogPath = "<?php echo $mosConfig_live_site;
-         ?>/components/com_realestatemanager/";
-  <?php
-        $newArr = explode(",", JText::_('_REALESTATE_MANAGER_HOUSE_MARKER'));
-        $numPick = '';
-        if (isset($newArr[$this->item->property_type])) {
-            $numPick = $newArr[$this->item->property_type];
-        }
-  ?>
-        var srcForPic = "<?php echo $numPick; ?>";
-        var image = '';
-        if(srcForPic.length){
-          var image = new google.maps.MarkerImage(imgCatalogPath + srcForPic,
-          new google.maps.Size(32, 32),
-          new google.maps.Point(0,0),
-          new google.maps.Point(16, 32));
-        }
-        var marker = new google.maps.Marker({ icon: image,position: myLatlng });
-        marker.setMap(map);
-      }
-    </script>
-    <?php }
-
-  }
+  //dibujar_mapa
 
 ?>
 <div id="overDiv" ></div>
@@ -1102,7 +967,7 @@ if ($realestatemanager_configuration['extra9'] == 1 && $this->item->extra9 > 0) 
     }else{
         $switchTranslateDayNight = JText::_('_REALESTATE_MANAGER_RENT_SPECIAL_PRICE_PER_NIGHT');    
     }
-    $query = "select * from #__rem_rent_sal WHERE fk_houseid='$this->item->id'";
+    $query = "select * from #__rem_rent_sal WHERE fk_houseid='{$this->item->id}'";
     $database->setQuery($query);
     $rentTerm = $database->loadObjectList();
         if(isset($rentTerm[0]->special_price)) { ?>
